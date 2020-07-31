4a18b0511b4ba6850341c098a19d1676ecd5c814
Hank <hassanien.kaissi@gmail.com>
2020-08-01 01:09:50 +0400

    removed unnecessary files

--- notepad.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-file = sys.argv[1]
-with open(file) as f:
-    print(f.read())
--- send_mail.py
+++ send_mail.py
@@ -2,27 +2,29 @@ import smtplib
 from email.message import EmailMessage
 
 
-email = 'python.hank@gmail.com'
-password = 'H8523574569512h'
+email = "EMAIL"
+password = "password"
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as mail:
 
     mail.login(email, password)
-    
+
     # constructing an email messege to be sent
     messege = EmailMessage()
-    messege['To'] = 'hassanien.kaissi@gmail.com'  # or a list or emails or a comma seperated string or emails
+    # or a list or emails or a comma seperated string or emails
+    messege['To'] = 'RECIVER'
     messege['From'] = email
     messege['Subject'] = 'Testing smtp mail'
-    messege.set_content('This is a test of the smtp python email sending\nwith an attachment')
-    
-    # adding an attachment
-    file = open('automate/pythex.org.PNG', 'rb')
-    attachment = file.read()
-    messege.add_attachment(attachment, maintype='application', subtype='octet-stream', filename='regex cheatsheet.png')
+    messege.set_content(
+        'This is a test of the smtp python email sending\nwith an attachment')
 
-    # sending HTML email 
+    # adding an attachment
+    file = open('FILE', 'rb')
+    attachment = file.read()
+    messege.add_attachment(attachment, maintype='application',
+                           subtype='octet-stream', filename='regex cheatsheet.png')
+
+    # sending HTML email
     html = '''<!DOCTYPE html><html><body><h1>This is an HTML Heading</h1><p>and its paragraph</p></body></html>'''
-    
 
     # if have a text and attachment and want to add html have to add the alternative to text part
     text_part, attachment_part = messege.iter_parts()
